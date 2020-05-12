66f5292b45423a2192d4d713901037773a16f0f7
Thomas B Homburg <thomas@madethis.dk>
2020-05-12 12:35:14 +0200

    Extract gpg key name to env var

--- lib/utils.sh
+++ lib/utils.sh
@@ -3,5 +3,7 @@
 # TODO: Replace with an asdf variable once asdf starts providing the plugin name
 # as a variable
 plugin_name() {
- basename "$(dirname "$(dirname "$0")")"
+  basename "$(dirname "$(dirname "$0")")"
 }
+
+ASDF_NODEJS_KEYRING=asdf-nodejs.gpg
