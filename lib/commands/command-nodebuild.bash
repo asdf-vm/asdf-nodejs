c4e335f872b248b6578cdc56bfdeeafb831c7721
Augusto Moura <me@augustomoura.com>
2021-11-23 23:08:14 -0300

    feat: modify install to use node-build behind the scenes

--- lib/commands/command-nodebuild.bash
+++ lib/commands/command-nodebuild.bash
@@ -13,20 +13,26 @@ if [ "$NODEJS_ORG_MIRROR" ]; then
   export NODE_BUILD_MIRROR_URL="$NODEJS_ORG_MIRROR"
 fi
 
-if [ "${ASDF_CONCURRENCY-}" ]; then
-  export MAKE_OPTS="${MAKE_OPTS:-} -j \"$ASDF_CONCURRENCY\""
+if [[ $ASDF_NODEJS_CONCURRENCY =~ ^[0-9]+$ ]]; then
+  export MAKE_OPTS="${MAKE_OPTS:-} -j$ASDF_NODEJS_CONCURRENCY"
+  export NODE_MAKE_OPTS="${NODE_MAKE_OPTS:-} -j$ASDF_NODEJS_CONCURRENCY"
 fi
 
 nodebuild="${ASDF_NODEJS_NODEBUILD:-$ASDF_NODEJS_NODEBUILD_HOME/bin/node-build}" 
+nodebuild_args=()
 
 if ! [ -x "$nodebuild" ]; then
   printf "Binary for node-build not found\n"
 
-  if ! [ "$ASDF_NODEJS_NODEBUILD" ]; then
+  if ! [ "${ASDF_NODEJS_NODEBUILD-}" ]; then
     printf "Are you sure it was installed? Try running \`asdf %s update-nodebuild\` to do a local update or install\n" "$(plugin_name)"
   fi
 
   exit 1
 fi
 
-exec "$nodebuild" "$@"
+if [ "${ASDF_NODEJS_VERBOSE_INSTALL-}" ]; then
+  nodebuild_args+=(-v)
+fi
+
+exec "$nodebuild" "${nodebuild_args[@]}" "$@"
